9bc6d8eca407ba94b38e883b1c9d7fbe7ea288a8
HughMorris01 <gfarrell2@Live.com>
2023-03-16 09:15:23 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Navbar
+const menu = document.querySelector('.menu');
+const navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () => {
+    navbar.classList.toggle('change');
+    menu.classList.toggle('change');
+})
+
+
+//End of Navbar
