eb2ee1ba4879ab5023b6734b54534055811d109d
HughMorris01 <gfarrell2@Live.com>
2023-03-16 16:10:58 -0400

    Add Pricing Cards Swiper

--- script.js
+++ script.js
@@ -6,6 +6,55 @@ menu.addEventListener('click', () => {
     navbar.classList.toggle('change');
     menu.classList.toggle('change');
 })
-
-
 //End of Navbar
+
+//Section 2 Video
+const video = document.querySelector('.video');
+const btn = document.querySelector('.buttons i');
+const bar = document.querySelector('.video-bar');
+
+const playPause = () => {
+    if(video.paused) {
+        video.play();
+        btn.className = 'far fa-pause-circle';
+        video.style.opacity = '.7';
+    } else {
+        video.pause();
+        btn.className = 'far fa-play-circle';
+        video.style.opacity = '.3';
+    }
+}
+
+btn.addEventListener('click', () => {
+    playPause();
+})
+
+video.addEventListener('timeupdate', ()=> {
+    const barWidth = video.currentTime / video.duration
+    console.log(barWidth)
+    bar.style.width = barWidth * 100 + "%";
+
+    if(video.ended){
+        btn.className = "far fa-play-circle"
+        video.style.opacity = '.3'
+        bar.style.width = 0;
+    }
+})
+
+//End of Section 2 Video
+
+// Section 3 Swiper
+var swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 45,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+});
+// Section 3 Swiper
